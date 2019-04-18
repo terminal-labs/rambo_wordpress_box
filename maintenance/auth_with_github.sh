50278ee41ad0993a11bf98cb40ec9a74a2ad8b6e
Michael Verhulst <michael@terminallabs.com>
2019-04-17 23:25:37 -0500

    Update auth_with_github.sh

--- maintenance/auth_with_github.sh
+++ maintenance/auth_with_github.sh
@@ -1,13 +1,8 @@
-mkdir -p ~/.ssh/ && echo -e "Host github.com\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
-git clone -b develop https://github.com/pallets/flask.git
-cd flask
-
-
-
 mkdir -p  ~/.ssh
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tFingerprintHash sha256\n" >  ~/.ssh/config
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tFingerprintHash sha256\n\tUserKnownHostsFile=/dev/null\n" >  /etc/ssh/ssh_config
 chmod 600  ~/.ssh
 ssh-keyscan rsa github.com >>  ~/.ssh/known_hosts
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 cat ~/.ssh/known_hosts
 cat  ~/.ssh/config
+git clone https://github.com/pallets/flask.git
