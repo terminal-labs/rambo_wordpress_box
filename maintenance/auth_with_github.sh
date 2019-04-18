2c535f7556252c90b5758ba5bdca8fd226fabd0e
Michael Verhulst <michael@terminallabs.com>
2019-04-18 00:03:34 -0500

    Update auth_with_github.sh

--- maintenance/auth_with_github.sh
+++ maintenance/auth_with_github.sh
@@ -1,12 +1,18 @@
-mkdir -p  /root/.ssh
+#echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >  /etc/ssh/ssh_config
+
 ssh-keygen -t rsa -N ""
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >  /etc/ssh/ssh_config
+mkdir -p  /root/.ssh
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >  /root/.ssh/config
-ssh-keyscan rsa github.com >>  /root/.ssh/known_hosts
 ssh-keyscan github.com >> /root/.ssh/known_hosts
 chmod 700 /root/.ssh
 chmod -R 600 /root/.ssh/
 chmod -R 600 /root/.ssh/id_rsa
 chmod -R 644 /root/.ssh/id_rsa.pub
-cat /root/.ssh/known_hosts
-cat  /root/.ssh/config
+
+mkdir -p  ~/.ssh
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >  ~/.ssh/config
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+chmod 700 ~/.ssh
+chmod -R 600 ~/.ssh/
+chmod -R 600 ~/.ssh/id_rsa
+chmod -R 644 ~/.ssh/id_rsa.pub
