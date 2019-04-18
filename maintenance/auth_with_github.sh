7a2a5c1ff759f347202e03830a778877fe31de73
Michael Verhulst <michael@terminallabs.com>
2019-04-18 00:21:53 -0500

    Update auth_with_github.sh

--- maintenance/auth_with_github.sh
+++ maintenance/auth_with_github.sh
@@ -1,12 +1,12 @@
-mkdir -p  ~/.ssh
+mkdir -p  /home/circleci/.ssh
 cd /home/circleci/.ssh
 rm *
 ssh-keygen -t rsa -N "" -f /home/circleci/.ssh/id_rsa 
-sudo echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >  ~/.ssh/config
+sudo echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >  /home/circleci/.ssh/config
 sudo echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >  /etc/ssh/sshd_config
-sudo ssh-keyscan github.com >> ~/.ssh/known_hosts
-sudo chmod 700 ~/.ssh
-sudo chmod -R 600 ~/.ssh/
+sudo ssh-keyscan github.com >> /home/circleci/.ssh/known_hosts
+sudo chmod 700 /home/circleci/.ssh
+sudo chmod -R 600 /home/circleci/.ssh/
 sudo chmod -R 600 /home/circleci/.ssh/id_rsa
 sudo chmod -R 644 /home/circleci/.ssh/id_rsa.pub
 cd ~
