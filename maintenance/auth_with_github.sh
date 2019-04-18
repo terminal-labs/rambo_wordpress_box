78967e2e8687ef57fe1763f22f7fb4fe7bc9207d
Michael Verhulst <michael@terminallabs.com>
2019-04-17 23:32:41 -0500

    Update auth_with_github.sh

--- maintenance/auth_with_github.sh
+++ maintenance/auth_with_github.sh
@@ -7,4 +7,3 @@ ssh-keyscan rsa github.com >>  ~/.ssh/known_hosts
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 cat ~/.ssh/known_hosts
 cat  ~/.ssh/config
-git clone https://github.com/pallets/flask.git
