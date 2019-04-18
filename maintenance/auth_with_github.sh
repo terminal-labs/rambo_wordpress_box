212756776f264d234d348d072f02845dec510b4a
Michael Verhulst <michael@terminallabs.com>
2019-04-18 00:07:34 -0500

    Update auth_with_github.sh

--- maintenance/auth_with_github.sh
+++ maintenance/auth_with_github.sh
@@ -1,15 +1,5 @@
-#echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >  /etc/ssh/ssh_config
-
-ssh-keygen -t rsa -N ""
-mkdir -p  /root/.ssh
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >  /root/.ssh/config
-ssh-keyscan github.com >> /root/.ssh/known_hosts
-chmod 700 /root/.ssh
-chmod -R 600 /root/.ssh/
-chmod -R 600 /root/.ssh/id_rsa
-chmod -R 644 /root/.ssh/id_rsa.pub
-
 mkdir -p  ~/.ssh
+ssh-keygen -t rsa -N ""
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >  ~/.ssh/config
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 chmod 700 ~/.ssh
