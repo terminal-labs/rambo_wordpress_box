3face70727e6a347f112b44c8acee013d43c8731
Michael Verhulst <michael@terminallabs.com>
2019-04-18 00:26:33 -0500

    Update auth_with_github.sh

--- maintenance/auth_with_github.sh
+++ maintenance/auth_with_github.sh
@@ -1,7 +1,7 @@
 mkdir -p  /home/circleci/.ssh
 cd /home/circleci/.ssh
 rm *
-ssh-keygen -t rsa -N "" -f /home/circleci/.ssh/id_rsa 
+sudo ssh-keygen -t rsa -N "" -f /home/circleci/.ssh/id_rsa 
 sudo echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >  /home/circleci/.ssh/config
 sudo ssh-keyscan github.com >> /home/circleci/.ssh/known_hosts
 sudo chmod 700 /home/circleci/.ssh
@@ -9,4 +9,4 @@ sudo chmod -R 600 /home/circleci/.ssh/
 sudo chmod -R 600 /home/circleci/.ssh/id_rsa
 sudo chmod -R 644 /home/circleci/.ssh/id_rsa.pub
 cd ~
-git clone https://github.com/pallets/flask.git
+sudo git clone https://github.com/pallets/flask.git
