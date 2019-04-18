1dcf96d7ff29bfdffbc240a09b9e7386d04a3030
Michael Verhulst <michael@terminallabs.com>
2019-04-17 21:37:59 -0500

    Update auth_with_github.sh

--- maintenance/auth_with_github.sh
+++ maintenance/auth_with_github.sh
@@ -1,5 +1,5 @@
-mkdir -p  /home/circleci/.ssh
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n" >  /home/circleci/.ssh/config
-chmod 600  /home/circleci/.ssh
-ssh-keyscan -Ht rsa github.com >>  /home/circleci/.ssh/known_hosts
-cat  /home/circleci/.ssh/config
+mkdir -p  ~/.ssh
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n" >  ~/.ssh/config
+chmod 600  ~/.ssh
+ssh-keyscan -Ht rsa github.com >>  ~/.ssh/known_hosts
+cat  ~/.ssh/config
