e014cf879403713cdbc843f0640ee85840af6a77
Michael Verhulst <michael@terminallabs.com>
2019-04-17 20:08:17 -0500

    Update install_salt_open.sh

--- maintenance/install_salt_open.sh
+++ maintenance/install_salt_open.sh
@@ -1,4 +1,3 @@
-ssh-keygen -F github.com || ssh-keyscan github.com >>~/.ssh/known_hosts
 wget -O bootstrap-salt.sh https://bootstrap.saltstack.com
 sudo sh bootstrap-salt.sh -P -X git v2018.3.4
 sudo rm bootstrap-salt.sh
