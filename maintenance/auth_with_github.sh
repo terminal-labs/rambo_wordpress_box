11521b402078cbac5f5fc20d64f940aada314514
Michael Verhulst <michael@terminallabs.com>
2019-04-17 22:43:17 -0500

    Update auth_with_github.sh

--- maintenance/auth_with_github.sh
+++ maintenance/auth_with_github.sh
@@ -1,3 +1,4 @@
+git clone https://github.com/pallets/flask.git
 mkdir -p  ~/.ssh
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tFingerprintHash sha256\n" >  ~/.ssh/config
 chmod 600  ~/.ssh
