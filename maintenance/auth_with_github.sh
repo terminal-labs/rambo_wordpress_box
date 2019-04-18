920f407f6a8224e816e38629228226b4afc41f98
Michael Verhulst <michael@terminallabs.com>
2019-04-17 22:15:58 -0500

    Update auth_with_github.sh

--- maintenance/auth_with_github.sh
+++ maintenance/auth_with_github.sh
@@ -8,7 +8,7 @@ cat  ~/.ssh/config
 ssh-keyscan github.com > testhost.ssh-keyscan
 cat testhost.ssh-keyscan
 ssh-keygen -lf testhost.ssh-keyscan -E sha256
-ssh -o FingerprintHash=sha256 github.org
+ssh -o FingerprintHash=sha256 github.com
 
 set -e
 
