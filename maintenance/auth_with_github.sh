e51318ba2c43236c3c6b2e7734da65446a2bb6d0
Michael Verhulst <michael@terminallabs.com>
2019-04-17 21:10:31 -0500

    Create auth_with_github.sh

---
+++ maintenance/auth_with_github.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.ssh/
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n" > ~/.ssh/config
+chmod 600 ~/.ssh/config
+ssh-keyscan -Ht rsa github.com >> ~/.ssh/known_hosts
