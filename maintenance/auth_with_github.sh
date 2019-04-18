25cd88d1fdccc49542de6ef73453e23cc4f0e300
Michael Verhulst <michael@terminallabs.com>
2019-04-18 00:27:17 -0500

    Update auth_with_github.sh

--- maintenance/auth_with_github.sh
+++ maintenance/auth_with_github.sh
@@ -8,5 +8,6 @@ sudo chmod 700 /home/circleci/.ssh
 sudo chmod -R 600 /home/circleci/.ssh/
 sudo chmod -R 600 /home/circleci/.ssh/id_rsa
 sudo chmod -R 644 /home/circleci/.ssh/id_rsa.pub
+cat /home/circleci/.ssh/known_hosts
 cd ~
 sudo git clone https://github.com/pallets/flask.git
