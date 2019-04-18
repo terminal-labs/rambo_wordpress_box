702bcf3771dcfc453a5c2434324cb842d68828a4
Michael Verhulst <michael@terminallabs.com>
2019-04-18 01:25:27 -0500

    Update auth_with_github.sh

--- maintenance/auth_with_github.sh
+++ maintenance/auth_with_github.sh
@@ -1,20 +1,20 @@
-mkdir -p /home/$USER/.ssh
-rm /home/$USER/.ssh/*
+mkdir -p /home/"$USER"/.ssh
+rm /home/"$USER"/.ssh/*
 
-sudo ssh-keygen -t rsa -N "" -f /home/$USER/.ssh/id_rsa 
-sudo echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >  /home/$USER/.ssh/config
-sudo ssh-keyscan github.com >> /home/$USER/.ssh/known_hosts
+sudo ssh-keygen -t rsa -N "" -f /home/"$USER"/.ssh/id_rsa 
+sudo echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" >  /home/"$USER"/.ssh/config
+sudo ssh-keyscan github.com >> /home/"$USER"/.ssh/known_hosts
 
-sudo chown -R $USER /home/$USER/.ssh
-sudo chgrp -R $USER /home/$USER/.ssh
-sudo chmod 700 /home/$USER/.ssh
-sudo chmod 644 /home/$USER/.ssh/authorized_keys
-sudo chmod 644 /home/$USER/.ssh/known_hosts
-sudo chmod 644 /home/$USER/.ssh/config
-sudo chmod 600 /home/$USER/.ssh/id_rsa
-sudo chmod 644 /home/$USER/.ssh/id_rsa.pub
+sudo chown -R "$USER" /home/"$USER"/.ssh
+sudo chgrp -R "$USER" /home/"$USER"/.ssh
+sudo chmod 700 /home/"$USER"/.ssh
+sudo chmod 644 /home/"$USER"/.ssh/authorized_keys
+sudo chmod 644 /home/"$USER"/.ssh/known_hosts
+sudo chmod 644 /home/"$USER"/.ssh/config
+sudo chmod 600 /home/"$USER"/.ssh/id_rsa
+sudo chmod 644 /home/"$USER"/.ssh/id_rsa.pub
 
-sudo cp -a /home/$USER/.ssh/. /root/.ssh/
+sudo cp -a /home/"$USER"/.ssh/. /root/.ssh/
 
 sudo chown -R root /root/.ssh
 sudo chgrp -R root /root/.ssh
