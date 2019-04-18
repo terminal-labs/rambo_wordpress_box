8b0fe77dc2dd40ee26a43cf1cd320fd7f366933e
Michael Verhulst <michael@terminallabs.com>
2019-04-17 21:47:45 -0500

    Update auth_with_github.sh

--- maintenance/auth_with_github.sh
+++ maintenance/auth_with_github.sh
@@ -1,6 +1,6 @@
 mkdir -p  ~/.ssh
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n" >  ~/.ssh/config
-echo -e "Host *\n\tStrictHostKeyChecking no\n >> ~/.ssh/config
+echo -e "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 chmod 600  ~/.ssh
 ssh-keyscan -Ht rsa github.com >>  ~/.ssh/known_hosts
 cat  ~/.ssh/config
