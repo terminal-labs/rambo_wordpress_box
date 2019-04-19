4144dee9fc53797257c31f567a7417744759d4d6
Michael Verhulst <michael@terminallabs.com>
2019-04-18 21:26:42 -0500

    Update mock_vagrant.sh

--- .circleci/mock_vagrant.sh
+++ .circleci/mock_vagrant.sh
@@ -2,7 +2,7 @@ useradd -m vagrant
 mkdir -p /home/vagrant
 touch /home/vagrant/.bashrc
 
-sed -i 's/^#\s*\(%wheel\s*ALL=(ALL)\s*NOPASSWD:\s*ALL\)/\1/' /etc/sudoers
+echo vagrant ALL=NOPASSWD:ALL > /etc/sudoers.d/vagrant
 
 mkdir -p /vagrant
 cp -a /home/circleci/repo/. /vagrant/
