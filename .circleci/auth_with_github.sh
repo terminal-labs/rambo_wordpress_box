e9b59efe75113c5f94961da81370a63c8986b68d
Michael Verhulst <michael@terminallabs.com>
2019-07-08 15:38:13 -0500

    Update auth_with_github.sh

--- .circleci/auth_with_github.sh
+++ .circleci/auth_with_github.sh
@@ -1,32 +1,50 @@
-USER=$(/usr/bin/id -run)
+mkdir -p /home/circleci/.ssh
+rm /home/circleci/.ssh/*
 
-mkdir -p /home/"$USER"/.ssh
-rm /home/"$USER"/.ssh/*
+su circleci <<'EOF'
+  ssh-keygen -t rsa -N "" -f /home/circleci/.ssh/id_rsa 
+  echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" > /home/circleci/.ssh/config
+  ssh-keyscan github.com >> /home/circleci/.ssh/known_hosts
+EOF
 
-ssh-keygen -t rsa -N "" -f /home/"$USER"/.ssh/id_rsa 
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" > /home/"$USER"/.ssh/config
-ssh-keyscan github.com >> /home/"$USER"/.ssh/known_hosts
+# circleci
+chown -R circleci /home/circleci/.ssh
+chgrp -R circleci /home/circleci/.ssh
+touch /home/circleci/.ssh/authorized_keys
 
-sudo touch /home/circleci/.ssh/authorized_keys
+chmod 755 /home/circleci
+chmod 700 /home/circleci/.ssh
+chmod 644 /home/circleci/.ssh/authorized_keys
+chmod 644 /home/circleci/.ssh/known_hosts
+chmod 644 /home/circleci/.ssh/config
+chmod 644 /home/circleci/.ssh/id_rsa.pub
+chmod 600 /home/circleci/.ssh/id_rsa
 
-sudo chown -R "$USER" /home/"$USER"/.ssh
-sudo chgrp -R "$USER" /home/"$USER"/.ssh
-sudo chmod 700 /home/"$USER"/.ssh
-sudo chmod 644 /home/"$USER"/.ssh/authorized_keys
-sudo chmod 644 /home/"$USER"/.ssh/known_hosts
-sudo chmod 644 /home/"$USER"/.ssh/config
-sudo chmod 600 /home/"$USER"/.ssh/id_rsa
-sudo chmod 644 /home/"$USER"/.ssh/id_rsa.pub
+# vagrant
+cp -a /home/circleci/.ssh/. /home/vagrant/.ssh/
 
-sudo cp -a /home/"$USER"/.ssh/. /root/.ssh/
+chown -R vagrant /home/vagrant/.ssh
+chgrp -R vagrant /home/vagrant/.ssh
+touch /home/vagrant/.ssh/authorized_keys
 
-sudo touch /root/.ssh/authorized_keys
+chmod 755 /home/vagrant
+chmod 700 /home/vagrant/.ssh
+chmod 644 /home/vagrant/.ssh/authorized_keys
+chmod 644 /home/vagrant/.ssh/known_hosts
+chmod 644 /home/vagrant/.ssh/config
+chmod 644 /home/vagrant/.ssh/id_rsa.pub
+chmod 600 /home/vagrant/.ssh/id_rsa
 
-sudo chown -R root /root/.ssh
-sudo chgrp -R root /root/.ssh
-sudo chmod 400 /root/.ssh
-sudo chmod 400 /root/.ssh/authorized_keys
-sudo chmod 400 /root/.ssh/known_hosts
-sudo chmod 400 /root/.ssh/config
-sudo chmod 400 /root/.ssh/id_rsa
-sudo chmod 400 /root/.ssh/id_rsa.pub
+# root
+cp -a /home/circleci/.ssh/. /root/.ssh/
+
+chown -R root /root/.ssh
+chgrp -R root /root/.ssh
+touch /root/.ssh/authorized_keys
+
+chmod 400 /root/.ssh
+chmod 400 /root/.ssh/authorized_keys
+chmod 400 /root/.ssh/known_hosts
+chmod 400 /root/.ssh/config
+chmod 400 /root/.ssh/id_rsa
+chmod 400 /root/.ssh/id_rsa.pub
