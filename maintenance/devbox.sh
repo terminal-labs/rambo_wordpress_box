3618c2d98d963f85a7ed4522e7b1fbcc4cfaf750
Michael Verhulst <michael@terminallabs.com>
2020-01-05 15:32:24 -0600

    Delete devbox.sh

--- maintenance/devbox.sh
+++
@@ -1,5 +0,0 @@
-vagrant up
-vagrant ssh --command 'cd /vagrant; sudo bash maintenance/install_deps.sh'
-vagrant ssh --command 'cd /vagrant; sudo bash maintenance/install_salt_open.sh'
-vagrant ssh --command 'cd /vagrant; sudo bash maintenance/run_salt.sh'
-vagrant ssh --command 'cd /vagrant; sudo bash maintenance/test.sh'
